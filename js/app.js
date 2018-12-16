99fba098e5c965dd60cbd473af4f3ba08f3eeeaa
zaidanki <44997660+zaidanki@users.noreply.github.com>
2018-12-16 01:59:41 -0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,12 +1,13 @@
 const phrasez = new Phrase(); // creating Phrase
 const game = new Game(); // creaing game
 let = keysPushed = [] // creating array for pushed keys
-
 function markButton() {
   const board = document.querySelector("#qwerty");
+  const boardChild = Array.from(board.children)
   board.addEventListener("click", e => {
     if (e.target.tagName === "BUTTON") {
       e.target.setAttribute("disabled", "");
+      console.log(e.target)
       game.handleInteraction(e.target.textContent);
       game.checkForWin();
     }
@@ -16,12 +17,17 @@ function markButton() {
     if (/^([a-z])/i.test(e.key)) {
       keysPushed.push(e.key);
       const keydn = e.key;
-      console.log(keysPushed);
       const newkey = keysPushed.filter(e => e === keydn).length;
+      const firstrow = Array.from(boardChild[0].children)
+      const secondrow = Array.from(boardChild[1].children)
+      const thirdrow = Array.from(boardChild[2].children)
+      const allRows = firstrow.concat(secondrow.concat(thirdrow)) // combining all arrays created from the keyrows qwerty
+      const keyDisable = allRows.filter(h => h.textContent == e.key)
+
+      keyDisable.forEach(e => e.setAttribute("disabled", "")) // this makes sure keys are disbaled on press
+
       console.log("newkey" + newkey);
-      if (newkey > 1) {
-        console.log("hi");
-      } else {
+      if (newkey > 1) {} else {
         game.handleInteraction(e.key);
         game.checkForWin();
       }
@@ -41,5 +47,5 @@ function resetDisplay() { // removes the starting display
   });
 }
 
-resetDisplay();// have to run it to make sure that the events are attached to buttons
+resetDisplay(); // have to run it to make sure that the events are attached to buttons
 markButton(); // run it to make sure events are attached.
